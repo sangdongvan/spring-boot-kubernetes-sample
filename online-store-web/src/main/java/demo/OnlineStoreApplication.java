4f781a7f0e9da90dc34a876117b24d3c3a676a02
Sang Dong <sang.dongvan@asnet.com.vn>
2018-01-21 23:55:38 +0700

    Implement Kubernetes manifests

--- online-store-web/src/main/java/demo/OnlineStoreApplication.java
+++ online-store-web/src/main/java/demo/OnlineStoreApplication.java
@@ -3,14 +3,12 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
 @EnableOAuth2Sso
 @EnableHystrix
