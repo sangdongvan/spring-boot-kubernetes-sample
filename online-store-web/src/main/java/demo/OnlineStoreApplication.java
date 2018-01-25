fbc39af2cd01b38e8bf1eedf3c8dfc4424d46f37
Sang Dong <sang.dongvan@asnet.com.vn>
2018-01-25 08:32:52 +0700

    Added discovery service

--- online-store-web/src/main/java/demo/OnlineStoreApplication.java
+++ online-store-web/src/main/java/demo/OnlineStoreApplication.java
@@ -3,6 +3,7 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 @EnableOAuth2Sso
 @EnableHystrix
 public class OnlineStoreApplication extends WebSecurityConfigurerAdapter {
