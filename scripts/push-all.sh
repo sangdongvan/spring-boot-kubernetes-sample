8551efd499c67c2d99fcbd6e3f1254c283e19dfd
Sang Dong <sang.dongvan@asnet.com.vn>
2019-04-09 18:42:21 +0700

    upgraded user-service and payment-service

--- scripts/push-all.sh
+++ scripts/push-all.sh
@@ -1,9 +1,9 @@
 docker push sangdongvan/istio-sample-online-store-web:0.1.3-SNAPSHOT
 docker push sangdongvan/istio-sample-catalog-service:0.1.3-SNAPSHOT
-docker push sangdongvan/istio-sample-payment-service:0.1.3-SNAPSHOT
+docker push sangdongvan/istio-sample-payment-service:0.1.4-SNAPSHOT
 docker push sangdongvan/istio-sample-edge-service:0.1.3-SNAPSHOT
 docker push sangdongvan/istio-sample-online-store-web:0.1.3-SNAPSHOT
-docker push sangdongvan/istio-sample-user-service:0.1.3-SNAPSHOT
+docker push sangdongvan/istio-sample-user-service:0.1.4-SNAPSHOT
 docker push sangdongvan/istio-sample-account-service:0.1.3-SNAPSHOT
 docker push sangdongvan/istio-sample-order-service:0.1.3-SNAPSHOT
 docker push sangdongvan/istio-sample-inventory-service:0.1.3-SNAPSHOT
